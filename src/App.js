aa4e132530614e0531cef5c4cf37600f12734182
Alina <63247458+AlinaTaran@users.noreply.github.com>
2020-12-10 23:43:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ class App extends Component {
     filter: '',
   };
 
+ componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+    
   addContact = (name, number) => {
     const { contacts } = this.state;
     if (
@@ -47,21 +61,7 @@ class App extends Component {
       contact.name.toLowerCase().includes(normalizedFilter),
     );
   };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+}
 
   render() {
     const { filter } = this.state;
